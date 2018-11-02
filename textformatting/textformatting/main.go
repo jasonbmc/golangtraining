301b676afde1824a7df989a3bd7abbe9722af730
Jason Mckim <jasonbmc10@gmail.com>
2018-11-01 21:46:14 -0400

    number formatting with loop

--- textformatting/textformatting/main.go
+++ textformatting/textformatting/main.go
@@ -7,4 +7,8 @@ func main() {
 	fmt.Printf("%d - %b \n", 42, 42)
 	fmt.Printf("%x - %b \n", 42, 42)
 	fmt.Printf("%#x - %b \n", 42, 42)
+
+	for i := 0; i < 200; i++ {
+		fmt.Printf("%d - %#x - %b - %x \n", i, i, i, i)
+	}
 }
