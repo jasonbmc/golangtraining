6343fb70ddc89543a3543d8595c1a10a7c026454
Jason Mckim <jasonbmc10@gmail.com>
2018-11-04 17:10:49 -0500

    number formatting, a loop, and formatting utf-8

--- textformatting/textformatting/main.go
+++ textformatting/textformatting/main.go
@@ -10,6 +10,12 @@ func main() {
 
 	//print decimal, hex, binary, oct
 	for i := 0; i < 200; i++ {
-		fmt.Printf("%d - %#x - %b - %x \n", i, i, i, i)
+		fmt.Printf("%d - %#x - %b - %x \n",
+			i, i, i, i)
+	}
+	//godoc.org/fmt - lists all the formatting options.
+	//deciaml, binary, hex, - seperated by tabs
+	for j := 0; j < 200; j++ {
+		fmt.Printf("%d \t %b \t %x \t %q \n", j, j, j, j)
 	}
 }
